df46df90e3a33012695850025452ac28ff776435
rikookazaki <riko.35.oka@gmail.com>
2024-10-18 12:26:46 +0900

    新規店舗登録画面作成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,11 @@ class RestaurantsController < ApplicationController
   def index
     
   end
+
+  def new
+    @restaurant = Restaurant.new
+    @countries = Country.all
+    @genres = Genre.all
+    @situations = Situation.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "restaurants#index"
+  resources :restaurants, only:[:new, :create]
 end
