7032c08533cfba7817b6699ef665ff2f51d0e3ad
rikookazaki <riko.35.oka@gmail.com>
2024-10-18 15:17:32 +0900

    店舗情報保存、表示

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
 
   def index
-    
+    @restaurants = Restaurant.all
   end
 
   def new
@@ -9,5 +9,21 @@ class RestaurantsController < ApplicationController
     @countries = Country.all
     @genres = Genre.all
     @situations = Situation.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to root_path
+    else 
+      render :new, status: :unprocessable_entitiy
+    end
+  end
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :access, :phone_num, :description, :opening_hours, :website, :payment, :country_ids, :genre_ids, :situation_ids, images:[] )
   end
 end
