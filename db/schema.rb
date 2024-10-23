96ace7869f78494b1f9115dc1fe188047a70e6a3
rikookazaki <riko.35.oka@gmail.com>
2024-10-23 15:27:20 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_18_050559) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_18_091655) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_18_050559) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "region"
   end
 
   create_table "genres", charset: "utf8mb3", force: :cascade do |t|
