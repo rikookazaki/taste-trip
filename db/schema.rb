66e7ab4a353777b587f33b4c64a05123db6df2b6
rikookazaki <riko.35.oka@gmail.com>
2024-10-29 18:12:48 +0900

    usersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_28_051655) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_29_085644) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -98,6 +98,21 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_28_051655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "item"
+  end
+
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.date "birthday"
+    t.integer "gender"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
