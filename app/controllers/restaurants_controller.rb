ac168009bf9d58592cf742afdd67e01b0ef08b70
rikookazaki <riko.35.oka@gmail.com>
2024-10-29 14:46:33 +0900

    削除機能

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -47,6 +47,11 @@ class RestaurantsController < ApplicationController
     end
   end
   
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to root_path
+  end
   
   
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,11 @@
 class Restaurant < ApplicationRecord
-  has_many :restaurant_countries
+  has_many :restaurant_countries, dependent: :destroy
   has_many :countries, through: :restaurant_countries
 
-  has_many :restaurant_genres
+  has_many :restaurant_genres, dependent: :destroy
   has_many :genres, through: :restaurant_genres
 
-  has_many :restaurant_situations
+  has_many :restaurant_situations, dependent: :destroy
   has_many :situations, through: :restaurant_situations
 
   has_many_attached :images
