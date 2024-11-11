53b2033923b9a835e321c737298d7bfe86a2e91d
rikookazaki <riko.35.oka@gmail.com>
2024-11-11 14:50:56 +0900

    jsボタンの色変化

--- app/javascript/color.js
+++ app/javascript/color.js
@@ -1,22 +1,37 @@
 function color() {
-  const submitButton = document.getElementById("initial-submit-btn")
-  const secondSubmitButton = document.getElementById("second-submit-btn")
-  
-  submitButton.addEventListener('mouseover', function(){
-    submitButton.setAttribute("style", "background-color:#f1c574;")
-  })
-  submitButton.addEventListener('mouseout', function(){
-    submitButton.removeAttribute("style")
-  })
+  const submitButton = document.getElementById("initial-submit-btn");
+  const secondSubmitButton = document.getElementById("second-submit-btn");
+  const linksToRoot = document.querySelectorAll(".link_to_root");
 
-  secondSubmitButton.addEventListener('mouseover', function(){
-    secondSubmitButton.setAttribute("style", "background-color:#f1c574;")
-  })
-  secondSubmitButton.addEventListener('mouseout', function(){
-    secondSubmitButton.removeAttribute("style")
-  })
+  if (submitButton) {
+    submitButton.addEventListener('mouseover', function() {
+      submitButton.setAttribute("style", "background-color:#f1c574;");
+    });
+    submitButton.addEventListener('mouseout', function() {
+      submitButton.removeAttribute("style");
+    });
+  }
 
+  if (secondSubmitButton) {
+    secondSubmitButton.addEventListener('mouseover', function() {
+      secondSubmitButton.setAttribute("style", "background-color:#f1c574;");
+    });
+    secondSubmitButton.addEventListener('mouseout', function() {
+      secondSubmitButton.removeAttribute("style");
+    });
+  }
+
+  linksToRoot.forEach(function(link) {
+    link.addEventListener('mouseover', function() {
+      link.setAttribute("style", "background-color:#f1c574;");
+    });
+    link.addEventListener('mouseout', function() {
+      link.removeAttribute("style");
+    });
+  });
 }
 
-window.addEventListener('turbo:load', color);
-window.addEventListener('turbo:render', color);
+// Turboと通常のDOMの読み込みイベントの両方でcolor関数を実行
+document.addEventListener("DOMContentLoaded", color);
+document.addEventListener("turbo:load", color);
+
