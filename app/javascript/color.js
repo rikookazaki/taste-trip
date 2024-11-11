0c80374f79fa260a6a81e842fcb7a27c7e7a96d9
rikookazaki <riko.35.oka@gmail.com>
2024-11-11 18:43:06 +0900

    お気に入りボタン修正

--- app/javascript/color.js
+++ app/javascript/color.js
@@ -1,7 +1,11 @@
 function color() {
-  const linksToRoot = document.querySelectorAll(".link_to_root");   //検索画面に戻るボタン
-  const headButton = document.querySelectorAll(".head_btn");        //ヘッダーのボタン
-  const submitButton = document.querySelectorAll(".submit_btn");    //検索・申請の送信ボタン
+  const linksToRoot = document.querySelectorAll(".link_to_root");          //検索画面に戻るボタン
+  const headButton = document.querySelectorAll(".head_btn");               //ヘッダーのボタン
+  const submitButton = document.querySelectorAll(".submit_btn");           //検索・申請の送信ボタン
+  const favoriteButton = document.querySelectorAll(".favorite-btn");       //お気に入りボタン
+  const restaurantName = document.querySelectorAll(".restaurant_name");    //店名
+
+
 
   linksToRoot.forEach(function(link) {
     link.addEventListener('mouseover', function() {
@@ -27,6 +31,24 @@ function color() {
     });
     btn.addEventListener('mouseout', function() {
       btn.removeAttribute("style");
+    });
+  });
+
+  favoriteButton.forEach(function(btn) {
+    btn.addEventListener('mouseover', function() {
+      btn.setAttribute("style", "background-color:#96e296;");
+    });
+    btn.addEventListener('mouseout', function() {
+      btn.removeAttribute("style");
+    });
+  });
+
+  restaurantName.forEach(function(name) {
+    name.addEventListener('mouseover', function() {
+      name.setAttribute("style", "color:#96e296;");
+    });
+    name.addEventListener('mouseout', function() {
+      name.removeAttribute("style");
     });
   });
 }
