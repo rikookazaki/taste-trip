23bbb11b0a32097956368c2e42733c6ff2c0f14d
rikookazaki <riko.35.oka@gmail.com>
2024-11-13 15:30:07 +0900

    デプロイ

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,4 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed:replant
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:seed:replant
