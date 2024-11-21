d8fce248564820371cbfeb2c32f5a1151cac795a
rikookazaki <riko.35.oka@gmail.com>
2024-11-21 22:25:57 +0900

    createアクション定義

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to @restaurant
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
